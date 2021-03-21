57574d2e62a5f501b988aa43df42e003049b84f8
Thea Choem <theacheng.g6@gmail.com>
2021-03-21 14:19:24 +0700

    design authentication and backup screen (#32)

--- lib/screens/ask_for_name_sheet.dart
+++ lib/screens/ask_for_name_sheet.dart
@@ -7,6 +7,7 @@ import 'package:write_story/models/user_model.dart';
 import 'package:write_story/notifier/user_model_notifier.dart';
 import 'package:write_story/screens/home_screen.dart';
 import 'package:write_story/widgets/vt_ontap_effect.dart';
+import 'package:write_story/widgets/vt_tab_view.dart';
 
 class AskForNameSheet extends HookWidget {
   const AskForNameSheet({Key? key}) : super(key: key);
@@ -22,11 +23,11 @@ class AskForNameSheet extends HookWidget {
     bool canContinue = nameNotEmpty;
     canContinue = nameNotEmpty && notifier.user?.nickname != notifier.nickname;
 
-    final _theme = Theme.of(context);
-
-    final _continueButton = buildContinueButton(
+    final tabController = useTabController(initialLength: 2);
+    final _continueButton = _buildContinueButton(
       nameNotEmpty: canContinue,
       context: context,
+      title: tr("common.continute_msg"),
       onTap: () async {
         final success = await notifier.setUser(
           UserModel(
@@ -60,195 +61,66 @@ class AskForNameSheet extends HookWidget {
                 kToolbarHeight) /
             constrant.maxHeight;
 
-        final child = Column(
+        final tab1 = Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                buildHeaderText(_theme, context),
-                const SizedBox(height: 24.0),
-                buildTextField(notifier, _theme, context),
-                TextButton(
-                  onPressed: () async {
-                    await notifier.wDatabase.generateBackup();
-                  },
-                  child: Text(
-                    "Generate backup",
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 1,
-                  ),
+                _buildHeaderText(
+                  context: context,
+                  title: tr("ask_for_name_sheet.hello_msg"),
+                  subtitle: tr("ask_for_name_sheet.msg"),
                 ),
-                for (var val in notifier.wDatabase.backups)
-                  TextButton(
-                    onPressed: () async {
-                      await notifier.wDatabase.restoreBackup(val);
-                    },
-                    child: Text(
-                      " $val",
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                    ),
-                  ),
+                const SizedBox(height: 24.0),
+                _buildTextField(
+                  context: context,
+                  hintText: tr("ask_for_name_sheet.hint_text"),
+                  initialValue:
+                      notifier.user != null ? notifier.user?.nickname : null,
+                  onChanged: (String value) {
+                    notifier.setNickname(value);
+                  },
+                ),
               ],
             ),
             _continueButton,
           ],
         );
+
+        final tab2 = WTab2();
         return DraggableScrollableSheet(
           initialChildSize: initHeight,
           maxChildSize: 1,
           minChildSize: 0.2,
           builder: (context, controller) {
             return Container(
-              child: child,
               height: double.infinity,
               decoration: buildBoxDecoration(context),
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16.0,
-                vertical: 32.0,
+              child: VTTabView(
+                controller: tabController,
+                children: [
+                  Container(
+                    child: tab1,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 32.0,
+                    ),
+                  ),
+                  Container(
+                    child: tab2,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 32.0,
+                    ),
+                  )
+                ],
               ),
             );
           },
         );
       },
-    );
-  }
-
-  Widget buildHeaderText(ThemeData _theme, BuildContext context) {
-    final _style =
-        _theme.textTheme.headline4?.copyWith(color: _theme.primaryColor);
-
-    return Container(
-      width: double.infinity,
-      child: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                tr("ask_for_name_sheet.hello_msg"),
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                textAlign: TextAlign.left,
-                style: _style,
-              ),
-              Text(
-                tr("ask_for_name_sheet.msg"),
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                textAlign: TextAlign.left,
-                style: _theme.textTheme.bodyText1,
-              ),
-            ],
-          ),
-          Positioned(
-            right: 0,
-            child: Container(
-              height: 38.0,
-              child: Row(
-                children: [
-                  VTOnTapEffect(
-                    onTap: () {
-                      context.setLocale(Locale("km"));
-                    },
-                    child: Image.asset("assets/flags/km-flag.png"),
-                  ),
-                  const SizedBox(width: 4.0),
-                  VTOnTapEffect(
-                    onTap: () {
-                      context.setLocale(Locale("en"));
-                    },
-                    child: Image.asset("assets/flags/en-flag.png"),
-                  ),
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget buildTextField(
-    UserModelNotifier notifier,
-    ThemeData _theme,
-    BuildContext context,
-  ) {
-    final _textTheme = _theme.textTheme;
-
-    final _style = _textTheme.subtitle1?.copyWith(
-      color: _textTheme.bodyText1?.color?.withOpacity(0.7),
-    );
-
-    final _hintStyle = _textTheme.subtitle1?.copyWith(
-      color: _theme.disabledColor,
-    );
-
-    final _decoration = InputDecoration(
-      contentPadding: const EdgeInsets.symmetric(horizontal: 2.0),
-      fillColor: _theme.scaffoldBackgroundColor,
-      hintText: tr("ask_for_name_sheet.hint_text"),
-      hintStyle: _hintStyle,
-      filled: true,
-      border: OutlineInputBorder(
-        borderSide: BorderSide.none,
-        borderRadius: BorderRadius.circular(10),
-      ),
-    );
-
-    return TextFormField(
-      cursorColor: Theme.of(context).primaryColor,
-      textAlign: TextAlign.center,
-      style: _style,
-      maxLines: 1,
-      initialValue: notifier.user != null ? notifier.user?.nickname : null,
-      decoration: _decoration,
-      onChanged: (String value) {
-        notifier.setNickname(value);
-      },
-    );
-  }
-
-  Widget buildContinueButton({
-    required bool nameNotEmpty,
-    required VoidCallback onTap,
-    required BuildContext context,
-  }) {
-    final _theme = Theme.of(context);
-
-    final _effects = [
-      VTOnTapEffectItem(
-        effectType: VTOnTapEffectType.touchableOpacity,
-        active: 0.5,
-      ),
-    ];
-
-    final _decoration = BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: nameNotEmpty
-            ? _theme.primaryColor
-            : _theme.scaffoldBackgroundColor);
-
-    return IgnorePointer(
-      ignoring: !nameNotEmpty,
-      child: VTOnTapEffect(
-        onTap: onTap,
-        effects: _effects,
-        child: AnimatedContainer(
-          duration: const Duration(milliseconds: 150),
-          height: 48,
-          decoration: _decoration,
-          alignment: Alignment.center,
-          child: Text(
-            tr("common.continute_msg"),
-            style: _theme.textTheme.bodyText1?.copyWith(
-              color: nameNotEmpty ? Colors.white : _theme.disabledColor,
-            ),
-          ),
-        ),
-      ),
     );
   }
 
@@ -272,4 +144,337 @@ class AskForNameSheet extends HookWidget {
       borderRadius: borderRadius,
     );
   }
+}
+
+class WTab2 extends StatelessWidget {
+  const WTab2({
+    Key? key,
+    this.isSignin = false,
+  }) : super(key: key);
+
+  final bool isSignin;
+
+  @override
+  Widget build(BuildContext context) {
+    final info =
+        "Email and password are used to identify your database. Please try not to forget it.";
+    final dbInfo =
+        "For your privacy, backup data will be encrypted before upload to our database.";
+
+    var loginInfo = _buildInfo(context, info);
+    var databaseInfo = _buildInfo(context, dbInfo);
+
+    if (!isSignin)
+      return buildSignInAuth(context, loginInfo);
+    else
+      return buildBackup(context, databaseInfo);
+  }
+
+  Column buildSignInAuth(BuildContext context, Row loginInfo) {
+    final _logButton = _buildContinueButton(
+      nameNotEmpty: true,
+      onTap: () {},
+      context: context,
+      title: "Log into your account",
+    );
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildHeaderText(
+              context: context,
+              title: "Setting",
+              subtitle: "Backup or restore your data from cloud.",
+              showLangs: false,
+            ),
+            Column(
+              children: [
+                const SizedBox(height: 24.0),
+                _buildTextField(
+                  context: context,
+                  hintText: "Your email",
+                  onChanged: (String value) {},
+                ),
+                const SizedBox(height: 12.0),
+                _buildTextField(
+                  context: context,
+                  hintText: "Your password",
+                  onChanged: (String value) {},
+                ),
+              ],
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            loginInfo,
+            const SizedBox(height: 16.0),
+            _logButton,
+          ],
+        ),
+      ],
+    );
+  }
+
+  Column buildBackup(BuildContext context, Row databaseInfo) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildHeaderText(
+              context: context,
+              title: "Setting",
+              subtitle: "Backup or restore your data from cloud.",
+              showLangs: false,
+            ),
+            const SizedBox(height: 8.0),
+            VTOnTapEffect(
+              onTap: () {},
+              child: Container(
+                height: 48,
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(horizontal: 12.0),
+                alignment: Alignment.centerLeft,
+                color: Theme.of(context).scaffoldBackgroundColor,
+                child: Text(
+                  "Restore last backup - 12 May 2020",
+                  maxLines: 1,
+                ),
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            VTOnTapEffect(
+              onTap: () {},
+              child: Container(
+                height: 48,
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(horizontal: 12.0),
+                alignment: Alignment.centerLeft,
+                color: Theme.of(context).scaffoldBackgroundColor,
+                child: Text(
+                  "Backup current database",
+                  maxLines: 1,
+                ),
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Row(
+              children: [
+                Text("Signed in: theacheng.g6@gmail.com"),
+                SizedBox(width: 8.0),
+                VTOnTapEffect(
+                  onTap: () {},
+                  child: Text(
+                    "Sign out",
+                    style: TextStyle(
+                      color: Theme.of(context).errorColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            databaseInfo,
+            const SizedBox(height: 16.0),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+Widget _buildHeaderText({
+  required BuildContext context,
+  required String title,
+  required String subtitle,
+  bool showLangs = true,
+}) {
+  final _theme = Theme.of(context);
+  final _style =
+      _theme.textTheme.headline4?.copyWith(color: _theme.primaryColor);
+
+  return Container(
+    width: double.infinity,
+    child: Stack(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.left,
+              style: _style,
+            ),
+            Text(
+              subtitle,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.left,
+              style: _theme.textTheme.bodyText1,
+            ),
+          ],
+        ),
+        if (showLangs)
+          Positioned(
+            right: 0,
+            child: Container(
+              height: 38.0,
+              child: Row(
+                children: [
+                  VTOnTapEffect(
+                    onTap: () {
+                      context.setLocale(Locale("km"));
+                    },
+                    child: Image.asset("assets/flags/km-flag.png"),
+                  ),
+                  const SizedBox(width: 4.0),
+                  VTOnTapEffect(
+                    onTap: () {
+                      context.setLocale(Locale("en"));
+                    },
+                    child: Image.asset("assets/flags/en-flag.png"),
+                  ),
+                  const SizedBox(width: 4.0),
+                  VTOnTapEffect(
+                    onTap: () {
+                      context.setLocale(Locale("en"));
+                    },
+                    child: Container(
+                      height: 38,
+                      width: 38,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColorDark,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        Icons.save,
+                        color: Theme.of(context).backgroundColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+      ],
+    ),
+  );
+}
+
+Widget _buildContinueButton({
+  required bool nameNotEmpty,
+  required VoidCallback onTap,
+  required BuildContext context,
+  required String title,
+}) {
+  final _theme = Theme.of(context);
+
+  final _effects = [
+    VTOnTapEffectItem(
+      effectType: VTOnTapEffectType.touchableOpacity,
+      active: 0.5,
+    ),
+  ];
+
+  final _decoration = BoxDecoration(
+      borderRadius: BorderRadius.circular(10),
+      color:
+          nameNotEmpty ? _theme.primaryColor : _theme.scaffoldBackgroundColor);
+
+  return IgnorePointer(
+    ignoring: !nameNotEmpty,
+    child: VTOnTapEffect(
+      onTap: onTap,
+      effects: _effects,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 150),
+        height: 48,
+        decoration: _decoration,
+        alignment: Alignment.center,
+        child: Text(
+          title,
+          style: _theme.textTheme.bodyText1?.copyWith(
+            color: nameNotEmpty ? Colors.white : _theme.disabledColor,
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+Row _buildInfo(BuildContext context, String info) {
+  return Row(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Icon(
+        Icons.info,
+        size: Theme.of(context).textTheme.bodyText2?.height,
+        color: Theme.of(context).primaryColorDark.withOpacity(0.75),
+      ),
+      const SizedBox(width: 8.0),
+      Container(
+        padding: EdgeInsets.zero,
+        width: MediaQuery.of(context).size.width - 16 * 2 - 8 - 24,
+        child: Text(
+          info,
+          style: Theme.of(context).textTheme.bodyText2?.copyWith(
+                color: Theme.of(context).primaryColorDark.withOpacity(0.75),
+              ),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget _buildTextField({
+  required BuildContext context,
+  String hintText = "",
+  String? initialValue = "",
+  required ValueChanged<String> onChanged,
+}) {
+  final _theme = Theme.of(context);
+  final _textTheme = _theme.textTheme;
+
+  final _style = _textTheme.subtitle1?.copyWith(
+    color: _textTheme.bodyText1?.color?.withOpacity(0.7),
+  );
+
+  final _hintStyle = _textTheme.subtitle1?.copyWith(
+    color: _theme.primaryColorDark.withOpacity(0.3),
+  );
+
+  final _decoration = InputDecoration(
+    contentPadding: const EdgeInsets.symmetric(horizontal: 2.0),
+    fillColor: _theme.scaffoldBackgroundColor,
+    hintText: hintText,
+    hintStyle: _hintStyle,
+    filled: true,
+    border: OutlineInputBorder(
+      borderSide: BorderSide.none,
+      borderRadius: BorderRadius.circular(10),
+    ),
+  );
+
+  return TextFormField(
+    autocorrect: false,
+    cursorColor: Theme.of(context).primaryColor,
+    textAlign: TextAlign.center,
+    style: _style,
+    maxLines: 1,
+    initialValue: initialValue,
+    decoration: _decoration,
+    onChanged: onChanged,
+  );
 }
