793d11049c9a22dbd283870a0059fc124215e857
theacheng <theacheng.g6@gmail.com>
2021-04-09 00:27:49 +0700

    add cache image to profile image

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -596,7 +597,7 @@ class SettingScreen extends HookWidget with DialogMixin, WSnackBar {
                       isCollapse ? 0 : _avatarSize,
                     ),
                     image: DecorationImage(
-                      image: NetworkImage(imageUrl),
+                      image: CachedNetworkImageProvider(imageUrl),
                       fit: BoxFit.cover,
                     ),
                   ),
